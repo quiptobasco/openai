90d8b4ff182c2f6424582361ee3eeabf463aca03
Tim Renken <quiptobasco@gmail.com>
2022-12-07 17:48:36 -0800

    oai backend

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 8008;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
