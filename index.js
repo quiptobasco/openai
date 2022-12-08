6ec47f788d64d225cb4b63afb5f2a9c229098776
Tim Renken <quiptobasco@gmail.com>
2022-12-07 18:20:57 -0800

    added the rest

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 
@@ -7,6 +8,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
